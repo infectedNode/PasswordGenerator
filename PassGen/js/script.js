74163506af1db4fe3710a84f63aef9217d967709
Shivam Sharma <38954435+Shivamdot@users.noreply.github.com>
2018-06-07 14:32:11 +0530

    Add files via upload

---
+++ PassGen/js/script.js
@@ -0,0 +1,15 @@
+console.log("script linked");
+var keyList = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*ABCDEFGHIJKLMNOPQRST";
+var temp = '';
+
+function generatePass(pLenght) {
+    temp = '';
+	for(var i=0;i<pLenght;i++){
+		temp += keyList.charAt(Math.floor(Math.random() * keyList.length)) 
+	}
+	return temp;
+}
+
+function populate(enterLenght){
+	document.passGen.output.value = generatePass(enterLenght);
+}
